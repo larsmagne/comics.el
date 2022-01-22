0a7d1daae89fe62f9a93940578df543f4f30fbb9
Lars Ingebrigtsen <larsi@gnus.org>
2022-01-22 18:13:32 +0100

    Fix byte-compilation warnings

--- comics.el
+++ comics.el
@@ -25,30 +25,35 @@
 
 ;;; Code:
 
+(defvar comics-data nil)
+(defvar comics-sort nil)
+(defvar comics-publisher nil)
+
 ;; URL for imprint
 ;; https://www.comics.org/search/advanced/process/?target=series&method=icontains&logic=False&keywords=&title=&feature=&job_number=&pages=&pages_uncertain=&script=&pencils=&inks=&colors=&letters=&story_editing=&first_line=&characters=&synopsis=&reprint_notes=&story_reprinted=&notes=&issues=&volume=&issue_title=&variant_name=&is_variant=&issue_date=&indicia_frequency=&price=&issue_pages=&issue_pages_uncertain=&issue_editing=&isbn=&barcode=&rating=&issue_notes=&issue_reprinted=&is_indexed=&order1=date&order2=date&order3=&start_date=&end_date=&updated_since=&pub_name=marvel&pub_notes=&brand_group=%s&brand_emblem=&brand_notes=&indicia_publisher=&is_surrogate=&ind_pub_notes=&series=&series_year_began=&series_notes=&tracking_notes=&issue_count=&is_comics=&color=&dimensions=&paper_stock=&binding=&publishing_format=&page=%d
 
 (defun comics-gather-data (publisher times)
   (let* ((url "https://www.comics.org/search/advanced/process/?ind_pub_notes=&rating=&pages_uncertain=&letters=&characters=&brand_group=&series=&binding=&indicia_frequency=&issue_notes=&synopsis=&colors=&keywords=&first_line=&tracking_notes=&job_number=&issues=&issue_date=&issue_reprinted=&dimensions=&title=&is_comics=&feature=&indicia_publisher=&pub_name=%s&is_indexed=&reprint_notes=&method=icontains&pub_notes=&inks=&issue_title=&end_date=&variant_name=&series_notes=&price=&barcode=&paper_stock=&volume=&brand_emblem=&pages=&isbn=&issue_pages_uncertain=&issue_pages=&order2=date&order3=&color=&order1=date&pencils=&target=series&publishing_format=&story_editing=&notes=&is_surrogate=&issue_count=&issue_editing=&start_date=&script=&updated_since=&logic=False&is_variant=&series_year_began=&brand_notes=&story_reprinted=&page=%d")
 	 (data
-	  (loop with dom
-		for page from 1 upto times
-		append
-		(with-current-buffer (url-retrieve-synchronously (format url publisher page))
-		  (goto-char (point-min))
-		  (setq dom (libxml-parse-html-region (point) (point-max)))
-		  (loop for line in (cdr (dom-by-tag (dom-by-tag dom 'table) 'tr))
-			for tds = (dom-non-text-children line)
-			collect
-			(list :publisher (string-trim (dom-texts (nth 1 tds)))
-			      :title (string-trim
-				      (replace-regexp-in-string
-				       "\\[.\\]" ""
-				       (dom-texts (nth 2 tds))))
-			      :url (dom-attr (dom-by-tag (nth 2 tds) 'a) 'href)
-			      :year (string-trim (dom-texts (nth 3 tds)))
-			      :issues (string-trim (dom-texts (nth 4 tds)))
-			      :date (string-trim (dom-texts (nth 6 tds)))))))))
+	  (cl-loop
+	   with dom
+	   for page from 1 upto times
+	   append
+	   (with-current-buffer (url-retrieve-synchronously (format url publisher page))
+	     (goto-char (point-min))
+	     (setq dom (libxml-parse-html-region (point) (point-max)))
+	     (cl-loop for line in (cdr (dom-by-tag (dom-by-tag dom 'table) 'tr))
+		      for tds = (dom-non-text-children line)
+		      collect
+		      (list :publisher (string-trim (dom-texts (nth 1 tds)))
+			    :title (string-trim
+				    (replace-regexp-in-string
+				     "\\[.\\]" ""
+				     (dom-texts (nth 2 tds))))
+			    :url (dom-attr (dom-by-tag (nth 2 tds) 'a) 'href)
+			    :year (string-trim (dom-texts (nth 3 tds)))
+			    :issues (string-trim (dom-texts (nth 4 tds)))
+			    :date (string-trim (dom-texts (nth 6 tds)))))))))
     (with-temp-buffer
       (pp data (current-buffer))
       (write-region (point-min) (point-max)
@@ -77,12 +82,12 @@
 	   (format "%s %3d %-10s %-15s %s\n"
 		   (comics-date elem)
 		   (comics-issues elem)
-		   (comics-limit (getf elem :missing "*") 10)
-		   (comics-limit (getf elem :publisher) 15)
-		   (getf elem :title))
+		   (comics-limit (cl-getf elem :missing "*") 10)
+		   (comics-limit (cl-getf elem :publisher) 15)
+		   (cl-getf elem :title))
 	   'data elem
 	   'face `(:foreground
-		   ,(let ((missing (getf elem :missing "")))
+		   ,(let ((missing (cl-getf elem :missing "")))
 		      (cond
 		       ((string-match "=" missing)
 			"orange")
@@ -109,18 +114,18 @@
     "august" "september" "october" "november" "december"))
 
 (defun comics-date (elem)
-  (let* ((year (getf elem :year))
-	 (date (getf elem :date))
+  (let* ((year (cl-getf elem :year))
+	 (date (cl-getf elem :date))
 	 (month (car (split-string date)))
-	 (nmonth (position (replace-regexp-in-string "[^a-zA-Z]" "" month)
-			   comics-english-months
-			   :test 'equalp)))
+	 (nmonth (cl-position (replace-regexp-in-string "[^a-zA-Z]" "" month)
+			      comics-english-months
+			      :test 'equalp)))
     (if nmonth
 	(format "%04s-%02d" year (1+ nmonth))
       (format "%04s   " year))))		    
 
 (defun comics-issues (elem)
-  (string-to-number (car (split-string (getf elem :issues)))))
+  (string-to-number (car (split-string (cl-getf elem :issues)))))
 
 (defvar comics-mode-map
   (let ((map (make-keymap)))
@@ -167,8 +172,8 @@
 		 (let ((e1 (get-text-property (car k1) 'data))
 		       (e2 (get-text-property (car k2) 'data)))
 		   (if comics-sort
-		       (string< (downcase (comics-canon (getf e1 :title)))
-				(downcase (comics-canon (getf e2 :title))))
+		       (string< (downcase (comics-canon (cl-getf e1 :title)))
+				(downcase (comics-canon (cl-getf e2 :title))))
 		     (string< (comics-date e1) (comics-date e2)))))))
   (setq comics-sort (not comics-sort)))
 
@@ -187,9 +192,9 @@ If NO-HAVE (the prefix), sort the no-haves first."
 		 (let ((e1 (get-text-property (car k1) 'data))
 		       (e2 (get-text-property (car k2) 'data)))
 		   (< (string-to-number
-			(car (split-string (getf e1 :issues ""))))
+			(car (split-string (cl-getf e1 :issues ""))))
 		       (string-to-number
-			(car (split-string (getf e2 :issues ""))))))))))
+			(car (split-string (cl-getf e2 :issues ""))))))))))
 
 (defun comics-canon (string)
   (replace-regexp-in-string "^\\(A \\|The \\)" "" string))
@@ -198,15 +203,15 @@ If NO-HAVE (the prefix), sort the no-haves first."
   (interactive)
   (let ((elem (get-text-property (point) 'data)))
     (with-temp-buffer
-      (insert (comics-canon (getf elem :title)))
+      (insert (comics-canon (cl-getf elem :title)))
       (copy-region-as-kill (point-min) (point-max))
       (message "Copied '%s'" (buffer-string)))))
 
 (defun comics-edit-missing ()
   (interactive)
   (let* ((elem (get-text-property (point) 'data))
-	 (missing (read-from-minibuffer (format "Missing issues for %s: " (getf elem :title))
-					(getf elem :missing "")))
+	 (missing (read-from-minibuffer (format "Missing issues for %s: " (cl-getf elem :title))
+					(cl-getf elem :missing "")))
 	 (inhibit-read-only t))
     (plist-put elem :missing missing)
     (comics-save)
@@ -216,7 +221,7 @@ If NO-HAVE (the prefix), sort the no-haves first."
 
 (defun comics-add (title year issues url)
   (interactive "sTitle: \nsYear: \nsIssues: \nsURL: ")
-  (let ((elem (list :publisher (getf (get-text-property (point) 'data)
+  (let ((elem (list :publisher (cl-getf (get-text-property (point) 'data)
 				     :publisher)
 		    :title title
 		    :url url
@@ -234,7 +239,7 @@ If NO-HAVE (the prefix), sort the no-haves first."
   (interactive)
   (let* ((elem (get-text-property (point) 'data))
 	 (total (string-to-number
-		 (car (split-string (getf elem :issues "")))))
+		 (car (split-string (cl-getf elem :issues "")))))
 	 (inhibit-read-only t))
     (plist-put elem :missing
 	       (if (= total 1)
@@ -249,7 +254,7 @@ If NO-HAVE (the prefix), sort the no-haves first."
   (interactive)
   (let* ((elem (get-text-property (point) 'data))
 	 (total (string-to-number
-		 (car (split-string (getf elem :issues "")))))
+		 (car (split-string (cl-getf elem :issues "")))))
 	 (inhibit-read-only t))
     (plist-put elem :missing "-")
     (comics-save)
@@ -261,8 +266,8 @@ If NO-HAVE (the prefix), sort the no-haves first."
   (interactive)
   (let* ((elem (get-text-property (point) 'data))
 	 (date (read-from-minibuffer (format "Month for %s: "
-						(getf elem :title))
-					(getf elem :date "")))
+						(cl-getf elem :title))
+					(cl-getf elem :date "")))
 	 (inhibit-read-only t))
     (plist-put elem :date date)
     (plist-put elem :year (car (last (split-string date))))
@@ -282,14 +287,14 @@ If NO-HAVE (the prefix), sort the no-haves first."
 (defun comics-visit ()
   (interactive)
   (let ((elem (get-text-property (point) 'data)))
-    (browse-url (format "https://comics.org%s" (getf elem :url)))))
+    (browse-url (format "https://comics.org%s" (cl-getf elem :url)))))
 
 (defun comics-visit-externally ()
   (interactive)
   (let ((elem (get-text-property (point) 'data))
 	(browse-url-browser-function
 	 browse-url-secondary-browser-function))
-    (browse-url (format "https://comics.org%s/covers/" (getf elem :url)))))
+    (browse-url (format "https://comics.org%s/covers/" (cl-getf elem :url)))))
 
 (defun comics-count ()
   (interactive)
@@ -304,38 +309,38 @@ If NO-HAVE (the prefix), sort the no-haves first."
 	(unread-series 0)
 	(ok 0))
     (dolist (elem comics-data)
-      (let ((missing (getf elem :missing ""))
+      (let ((missing (cl-getf elem :missing ""))
 	    (total (string-to-number
-		    (car (split-string (getf elem :issues ""))))))
-	(incf totes total)
+		    (car (split-string (cl-getf elem :issues ""))))))
+	(cl-incf totes total)
 	(cond
 	 ((string-match "=" missing)
-	  (incf pending)
+	  (cl-incf pending)
 	  (let ((have (comics-parse-shipping missing)))
-	    (incf shipping (car have))
-	    (incf no-got (cadr have))
-	    (incf got (- total (car have) (cadr have)))))
+	    (cl-incf shipping (car have))
+	    (cl-incf no-got (cadr have))
+	    (cl-incf got (- total (car have) (cadr have)))))
 	 ((equal missing "+")
-	  (incf read total)
-	  (incf hidden))
+	  (cl-incf read total)
+	  (cl-incf hidden))
 	 ((equal missing "-")
-	  (incf ok)
-	  (incf unread-series)
-	  (incf got total))
+	  (cl-incf ok)
+	  (cl-incf unread-series)
+	  (cl-incf got total))
 	 ((equal missing "!")
-	  (incf read total)
-	  (incf ok)
-	  (incf got total))
+	  (cl-incf read total)
+	  (cl-incf ok)
+	  (cl-incf got total))
 	 ((equal missing "")
-	  (incf unread-series)
-	  (incf missings)
-	  (incf no-got total))
+	  (cl-incf unread-series)
+	  (cl-incf missings)
+	  (cl-incf no-got total))
 	 (t
 	  (let ((no-have (cadr (comics-parse-shipping missing))))
-	    (incf got (- total no-have))
-	    (incf no-got no-have))
-	  (incf unread-series)
-	  (incf missings)))))
+	    (cl-incf got (- total no-have))
+	    (cl-incf no-got no-have))
+	  (cl-incf unread-series)
+	  (cl-incf missings)))))
     (message "%d have, %d shipping, %d no-have, %d read, %d unread, %d total issues,\n%d ok, %d pending, %d hidden, %d missing, %d unread, %d total series"
 	     got shipping no-got read (- got read) totes
 	     ok pending hidden missings unread-series (+ pending missings ok))))
@@ -358,8 +363,8 @@ signifies that the number/range/parenthesised collection has been ordered."
 			  (concat elem "="))
 			(split-string sub "[, ]" t)
 			", "))))
-	    (incf shipping (car elem))
-	    (incf no-shipping (cadr elem)))))
+	    (cl-incf shipping (car elem))
+	    (cl-incf no-shipping (cadr elem)))))
       (dolist (elem (split-string (buffer-string) ", " t))
 	(let* ((range (mapcar
 		       'string-to-number
@@ -369,8 +374,8 @@ signifies that the number/range/parenthesised collection has been ordered."
 			  (1+ (- (cadr range) (car range)))
 			1)))
 	  (if (string-match "=$" elem)
-	      (incf shipping count)
-	    (incf no-shipping count))))
+	      (cl-incf shipping count)
+	    (cl-incf no-shipping count))))
       (list shipping no-shipping))))
 
 (defvar comics-marks nil)
@@ -402,20 +407,21 @@ signifies that the number/range/parenthesised collection has been ordered."
   (comics-save))
 
 (defun comics-update (elem)
-  (save-excursion
-    (goto-char (point-min))
-    (while (and (not (eobp))
-		(setq current (get-text-property (point) 'data))
-		(not (and (equal (plist-get current :url)
-				 (plist-get elem :url))
-			  (equal (plist-get current :title)
-				 (plist-get elem :title)))))
-      (forward-line 1))
-    (when (get-text-property (point) 'data)
-      (let ((inhibit-read-only t))
-	(delete-region (line-beginning-position)
-		       (line-beginning-position 2))
-	(comics-line elem)))))
+  (let ((current nil))
+    (save-excursion
+      (goto-char (point-min))
+      (while (and (not (eobp))
+		  (setq current (get-text-property (point) 'data))
+		  (not (and (equal (plist-get current :url)
+				   (plist-get elem :url))
+			    (equal (plist-get current :title)
+				   (plist-get elem :title)))))
+	(forward-line 1))
+      (when (get-text-property (point) 'data)
+	(let ((inhibit-read-only t))
+	  (delete-region (line-beginning-position)
+			 (line-beginning-position 2))
+	  (comics-line elem))))))
 
 (defun comics-search-mile-high ()
   (interactive)
@@ -424,7 +430,7 @@ signifies that the number/range/parenthesised collection has been ordered."
 	 browse-url-secondary-browser-function))
     (browse-url
      (format "https://www.milehighcomics.com/mcgi-bin/search.cgi?title=%s"
-	     (comics-canon (getf elem :title))))))
+	     (comics-canon (cl-getf elem :title))))))
 
 (defun comics-search-all ()
   (interactive)
@@ -433,13 +439,13 @@ signifies that the number/range/parenthesised collection has been ordered."
 	 browse-url-secondary-browser-function))
     (browse-url
      (format "https://www.milehighcomics.com/mcgi-bin/search.cgi?title=%s"
-	     (comics-canon (getf elem :title))))
+	     (comics-canon (cl-getf elem :title))))
     (browse-url
      (format "https://www.mycomicshop.com/search?q=%s"
-	     (comics-canon (getf elem :title))))
+	     (comics-canon (cl-getf elem :title))))
     (browse-url
      (format "https://www.ebay.com/sch/m.html?_ssn=cyberspacecomics&LH_PrefLoc=&_from=R40&_trksid=p2499338.m570.l1313.TR12.TRC2.A0.H0.Xpteranoman.TRS0&_nkw=%s&_sacat=0"
-	     (comics-canon (getf elem :title))))))
+	     (comics-canon (cl-getf elem :title))))))
 
 (defun comics-search (string)
   (interactive "sSearch for: ")
